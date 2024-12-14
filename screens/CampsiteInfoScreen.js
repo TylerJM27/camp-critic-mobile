222b815611135070b3229c8ff15e6bbb98b4dc59
Tyler Miller <tjm2793@gmail.com>
2024-12-14 00:52:11 -0800

    CampsiteInfoScreen

---
+++ screens/CampsiteInfoScreen.js
@@ -0,0 +1,10 @@
+import RenderCampsite from '../features/campsites/RenderCampsite';
+
+
+const CampsiteInfoScreen = (props) => {
+  return (
+    <RenderCampsite campsite={props.campsite} />
+  )
+}
+
+export default CampsiteInfoScreen;
